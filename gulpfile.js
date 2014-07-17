dd806149b3b21a94afd93b7b2f61e8384a0ce30a
tatelucas <tate.lucas@gmail.com>
2014-07-17 13:02:33 -0400

    instructions page

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 var svg = svgSprites.svg;
 
+var replacementtext = '<ol><li>Find the icon you would like to use in the project. Make sure to double check that we haven\'t already included it. You can view the active fonts here: <a href="http://milacron.crossroads.net/icons/preview-svg.html">http://milacron.crossroads.net/icons/preview-svg.html</a></li><li>Copy the SVG file for the icon you want to use in to the "/app/icons/" directory.</li><li>Run \'gulp\'.</li><li>Your icon will now be added to our sprite and you can use it in the project. Grab your icon code snippet by clicking "Show Snippet".</ol>'
+
 gulp.task('default', function() {
 
   gulp.src(['assets/preview-svg.html'])
     .pipe(replace('background: black;', 'background: black;fill:white;'))
     .pipe(replace('css/sprites.css', 'assets/css/sprites.css'))
-    //.pipe(replace(/<ol>.*<\/ol>/im, ''))
+    .pipe(replace('Files Generated:', 'Instructions for using the Crossroads approved icons.'))
+    .pipe(replace(/<ol>(?:.|\n)*<\/ol>/gi, replacementtext))
     .pipe(rename("index.html"))
     .pipe(gulp.dest('.'));
 
@@ -18,4 +21,3 @@ gulp.task('default', function() {
     .pipe(gulp.dest("assets"));
 
 });
-
